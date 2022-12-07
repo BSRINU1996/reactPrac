9b28f2ba4b6e3b8990570a8af0a8a003d42b29f0
MeSrinu <balisrinu1996@gmail.com>
2022-12-06 22:43:47 -0800

    my raect practise

---
+++ src/components/tryComponent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import FirstComponent  from "../firstComponent";
+
+
+class Srinu extends React.Component  {
+    render () {
+        return(
+            <div>
+                <h1>hello iam srinu!</h1>
+                <h2>hello iam srinu!</h2>
+                <h3>hello iam srinu!</h3>
+                <h4>hello iam srinu!</h4>
+                <h5>hello iam srinu!</h5>
+                <h6>hello iam srinu!</h6>
+                <FirstComponent />
+                
+            </div>
+        ) 
+    }
+}
+export default Srinu;
---
+++ src/firstComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class FirstComponent extends React.Component {
+    render() {
+        return(
+            <div id="main">
+                <h1> Hi This is Our New and First Compoent</h1>
+            </div>
+        )
+    }
+}
+
+
+export default FirstComponent;
+
+
